d7005caea9be9e33524910a52fc4d1d85b196690
guijun13 <guilhermejun13.grillo@gmail.com>
2021-06-24 01:19:55 -0300

    :sparkles: Add orange cards to homepage

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { Container, Row, Card, Button, Carousel } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCreditCard,
+  faPercent,
+  faShippingFast,
+  faStar,
+} from '@fortawesome/free-solid-svg-icons';
 import styles from '../src/theme/pages/index.module.css';
 import Header from '../src/components/Header';
 import Footer from '../src/components/Footer';
@@ -32,6 +39,33 @@ export default function Home() {
             />
           </Carousel.Item>
         </Carousel>
+
+        <Container className={styles.cardsContainer}>
+          <Card className={styles.cardItem}>
+            <Card.Body className={styles.cardBody}>
+              <p>Grande variedade de marcas</p>
+              <FontAwesomeIcon height="50px" icon={faStar} />
+            </Card.Body>
+          </Card>
+          <Card className={styles.cardItem}>
+            <Card.Body className={styles.cardBody}>
+              <p>Frete grátis em todo o Brasil</p>
+              <FontAwesomeIcon height="50px" icon={faShippingFast} />
+            </Card.Body>
+          </Card>
+          <Card className={styles.cardItem}>
+            <Card.Body className={styles.cardBody}>
+              <p>Maior a compra maior o desconto</p>
+              <FontAwesomeIcon height="50px" icon={faPercent} />
+            </Card.Body>
+          </Card>
+          <Card className={styles.cardItem}>
+            <Card.Body className={styles.cardBody}>
+              <p>Parcelamento em até 6x sem juros</p>
+              <FontAwesomeIcon height="50px" icon={faCreditCard} />
+            </Card.Body>
+          </Card>
+        </Container>
       </Container>
 
       <Footer />
