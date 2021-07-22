33ed93625b4d7eb341837edd4b841362e8227e56
guilherme.tavares <guilherme.tavares@wr.sc.usp.br>
2021-07-22 15:20:41 -0300

    Updated DB link and 'models' folder name

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,10 +8,10 @@ const app = express();
 const router = express.Router();
 
 // Conecta ao MongoDB
-mongoose.connect('mongodb+srv://gant:nome@cluster0.q87wq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',  {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://caocuidado:caocuidado@cluster0.onfiq.mongodb.net/petshopDB?retryWrites=true&w=majority',  {useNewUrlParser: true, useUnifiedTopology: true});
 
 // Carrega os modelos
-const Animal = require('./model/animal');
+const Animal = require('./models/animal');
 
 // Carega as Rotas
 const indexRoute = require('./routes/index-route');
