c495cbb3ec400aa459cb1a52a19698a86bb30a4d
Gustavo-de-Oliveira <gustavo_de_oliveira@usp.br>
2021-07-26 19:39:02 -0300

    inicio integração back

---
+++ frontend/pages/api/user.tsx
@@ -0,0 +1,13 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import connect from '../../utils/database';
+
+interface ResponseType {
+	message: string;
+}
+
+export default async (req: NextApiRequest, res: NextApiResponse<ResponseType>): Promise<void> => {
+	const { db } = await connect();
+	const response = await db.collection('user')
+
+	res.status(200).json(response.ops[0]);
+}
